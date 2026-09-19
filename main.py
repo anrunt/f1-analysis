a21ef8740048556006423f853e011583051ac2ae
anrunt <grunt.janek@gmail.com>
2026-09-19 14:50:48 +0200

    Comparing lap times and calculating delta

--- main.py
+++ main.py
@@ -44,12 +44,42 @@ class Lap(BaseModel):
    segments_sector_2: list[int | None] | None
    segments_sector_3: list[int | None] | None
 
+def fetch_laps(session_key: int, driver_number: int) -> list[Lap]:
+    lap_params = {"session_key": session_key, "driver_number": driver_number}
+    laps = httpx.get("https://api.openf1.org/v1/laps", params=lap_params, timeout=10)
+
+    laps.raise_for_status()
+
+    try:
+        validate_laps = TypeAdapter(list[Lap]).validate_json(laps.content)
+    except ValidationError as error:
+        raise ValueError("Error when parsing lap data") from error
+
+    print(f"Downloaded {len(validate_laps)} laps for driver number {lap_params["driver_number"]}")
+    return validate_laps
+
+def find_fastest_lap(laps: list[Lap]) -> Lap | None:
+    fastest_lap: Lap | None = None
+    for lap in laps:
+        if lap.lap_duration is not None and lap.lap_duration > 0 and lap.is_pit_out_lap == False:
+            if fastest_lap == None:
+                fastest_lap = lap
+            else:
+                if fastest_lap.lap_duration is not None and fastest_lap.lap_duration > lap.lap_duration:
+                    fastest_lap = lap
+
+    return fastest_lap
 
 def main():
     session_params = {
         "country_name": "Italy",
         "session_name": "Qualifying",
         "year": 2024,
+    }
+
+    drivers = {
+        "Norris" : 4,
+        "Piastri": 81
     }
 
     sessions = httpx.get(
@@ -80,35 +110,34 @@ def main():
         print("No qualifying session found for Monza in 2024.")
         return
 
-    lap_params = {"session_key": selected_session.session_key, "driver_number": 4}
-    laps = httpx.get("https://api.openf1.org/v1/laps", params=lap_params)
 
-    laps.raise_for_status()
+    drivers_fastest_lap: dict[str, Lap] = {}
+    for name, number in drivers.items():
+        laps = fetch_laps(selected_session.session_key, number)
+        fastest_lap = find_fastest_lap(laps)
 
-    try:
-        validate_laps = TypeAdapter(list[Lap]).validate_json(laps.content)
-    except ValidationError as error:
-        print("Error when parsing lap data: ", error)
-        return
+        if fastest_lap == None:
+            print(f"{name} has not set fastest_lap")
+            return
 
-    print(f"Downloaded {len(validate_laps)} laps for driver number {lap_params["driver_number"]}")
+        drivers_fastest_lap[name] = fastest_lap
 
-    fastest_lap: Lap | None = None
-    for lap in validate_laps:
-        if lap.lap_duration and lap.lap_duration > 0 and lap.is_pit_out_lap == False:
-            if fastest_lap == None:
-                fastest_lap = lap
-            else:
-                if fastest_lap.lap_duration is not None and fastest_lap.lap_duration > lap.lap_duration:
-                    fastest_lap = lap
+    
+    (first_driver, first_driver_lap), (second_driver, second_driver_lap) = drivers_fastest_lap.items()
 
-    if fastest_lap == None:
-        print("No fastest lap")
-        return
+    assert first_driver_lap.lap_duration is not None
+    assert second_driver_lap.lap_duration is not None
 
-    print(f"Fastest lap: {fastest_lap.lap_duration}")
+    delta = first_driver_lap.lap_duration - second_driver_lap.lap_duration
 
-
+    print(f"{first_driver} fastest lap: {first_driver_lap.lap_duration:.3f} seconds lap number: {first_driver_lap.lap_number}")
+    print(f"{second_driver} fastest lap: {second_driver_lap.lap_duration:.3f} seconds lap number: {second_driver_lap.lap_number}")
+    if delta < 0:
+        print(f"{first_driver} was faster than {second_driver} by {abs(delta):.3f} seconds")
+    elif delta > 0:
+        print(f"{second_driver} was faster than {first_driver} by {abs(delta):.3f} seconds")
+    elif delta == 0:
+        print(f"{first_driver} and {second_driver} had the same lap time of {first_driver_lap.lap_duration:.3f} seconds")
 
 if __name__ == "__main__":
     main()
